d0fabe0e0e1ba1f7e4ae1533a749b757c28550e6
leszko <rafal.leszko@gmail.com>
2016-02-13 23:00:37 +0100

    Add exercise5.

---
+++ src/main/java/com/luxoft/java7se/exercise5/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.luxoft.java7se.exercise5;
+
+public class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        MessagePrinter messagePrinter = new MessagePrinter();
+        System.out.println(messagePrinter.sayHello());
+    }
+}
