d16aec89911e5316a9569bf0a3963d70b1ecc8d6
leszko <rafal.leszko@gmail.com>
2016-02-13 20:58:37 +0100

    Add exercise1/

---
+++ src/main/java/com/luxoft/java7se/exercise1/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.luxoft.java7se.exercise1;
+
+/**
+ * This is a simplest class which prints Hello, world message.
+ */
+public class HelloWorld {
+    /**
+     * The main() method.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
