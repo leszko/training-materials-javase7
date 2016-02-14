cd2da5d18f4c6f512d8e82009f80b61d8b5646ed
leszko <rafal.leszko@gmail.com>
2016-02-14 01:06:58 +0100

    Add exercise6.

---
+++ src/main/java/com/luxoft/java7se/exercise6/Arrays.java
@@ -0,0 +1,13 @@
+package com.luxoft.java7se.exercise6;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        System.out.println(array[2]);
+
+        array = new int[]{1, 2, 3, 4, 5};
+        System.out.println(array.length);
+
+        System.out.println(array[5]);
+    }
+}
